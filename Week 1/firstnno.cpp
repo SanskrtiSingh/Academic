a7d8028b45c3ea89c142c64d88f4cbc6d10d9384
Sanskrti <taehyungkoyal@gmail.com>
2023-02-03 20:55:25 +0530

    week1-cpp3 cipherSchool

--- Week 1/firstnno.cpp
+++ Week 1/firstnno.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int FirstN(int num){
+int FirstNo(int num){
     if(num==1){
         return 1;
     }
 
-    return num+FirstN(num-1);
+    return num+FirstNo(num-1);
 }
 int main(){
-    int ans=FirstN(5);
+    int ans=FirstNo(5);
     cout<<ans<<endl;
 
     return 0;
